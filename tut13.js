4d2c4756785cdccc5de19c714f965147bc745cb4
KUMAR7BITTU <bittukumar30dec2002@gmail.com>
2025-10-21 20:09:11 +0530

    string

---
+++ tut13.js
@@ -0,0 +1,38 @@
+// Introduction to Strings in JavaScript 
+
+/*
+let name = "Harry";
+console.log(name.length);
+console.log(name[0]);
+console.log(name[1]);
+console.log(name[2]);
+*/
+
+/*
+let myfriend = "Prakash";
+console.log(myfriend);
+*/
+
+// We can insert variables directly in template literal . This is called string Interpolation .
+/*
+let boy1 = "Rakesh";
+let boy2 = "Nikhil";
+let sentence = `${boy1} is a friend of ${boy2}`;
+console.log(sentence);
+*/
+// Escape Sequence Characters in String
+let fruit1 = 'Bana\'na';
+console.log(fruit1);
+
+let fruit2 = "Bana\"na";
+console.log(fruit2);
+
+/*
+/n -> new line
+/t -> tab space 
+/r -> carriage return 
+*/
+
+// console.log("My name is \n Rakesh.")
+// console.log("My name is \t Rakesh.")
+console.log("My name is \r Rakesh.")
