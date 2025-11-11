fab1318d0290c777cf6c0a8f1fc9e22520e0a3a9
KUMAR7BITTU <bittukumar30dec2002@gmail.com>
2025-11-11 15:16:40 +0530

    Array methods

---
+++ tut17.js
@@ -0,0 +1,35 @@
+// String method
+
+let num = [149,2,3,34,4]
+// let b = num.toString()// to convert array into string .
+// console.log(num)
+// console.log(b)
+// console.log(typeof b) // string
+// console.log(typeof num) // object
+
+// let c = num.join("_")
+// console.log(c,typeof c)
+
+// let d = num.join(" and ")
+// console.log(d,typeof d)
+
+// let e = num.pop();
+// pop will make changes in the original array .
+// pop will return the poped element .
+// console.log(num);
+// console.log(e);
+
+// push will make changes in the original array .
+let f = num.push(90);
+console.log(num)
+console.log(f)
+
+// shift will remove the first element and also return it . It will make changes in the original array .
+// let g = num.shift();
+// console.log(num);
+// console.log(g);
+
+// unshift will add the element to the front of the array and return the length .
+// let h = num.unshift(311);
+// console.log(num);
+// console.log(h);
