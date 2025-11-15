f8a9318dbc1af2dbf42ec0551f7b151e4dd6db9b
KUMAR7BITTU <bittukumar30dec2002@gmail.com>
2025-11-15 10:11:23 +0530

    DOM, BOM & Window Object

---
+++ tut_28/script.js
@@ -0,0 +1,8 @@
+//console.log(window);
+
+// console is also present inside window
+// window.console.log(window)
+
+// console.log(document);
+console.log(document.body);
+document.body.style.background = "yellow";
