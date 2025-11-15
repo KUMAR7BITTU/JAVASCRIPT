9992c8c7967d79fcc4d8260fdcbaae954569d527
KUMAR7BITTU <bittukumar30dec2002@gmail.com>
2025-11-15 18:47:52 +0530

    Walking the DOM

---
+++ tut_31/script.js
@@ -0,0 +1,13 @@
+document.documentElement // This will give whole html tag .
+typeof(document.documentElement); // object
+
+document.head // This will give head tag .
+typeof(document.head) // object 
+
+document.body // This will give body tag .
+typeof(document.body) // object
+
+document.title = "Coder, Coffee and Bugs" // This will change the title 
+typeof(document.title) // string 
+
+// This will change the HTML content of user side not the server side .
