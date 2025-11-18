a257f9b9e5aa599c47e17eeeda69e411bc8e2410
KUMAR7BITTU <bittukumar30dec2002@gmail.com>
2025-11-18 19:03:19 +0530

    How to search DOM ?

---
+++ tut_36/script.js
@@ -0,0 +1,22 @@
+let ctitle1 = document.getElementById("firstcardtitle");
+ctitle1.style.color = "blue";
+
+// let ctitle2 = document.getElementsByClassName("card-title")[0];
+// ctitle2.style.color = "red";
+
+// let ctitle3 = document.querySelectorAll(".card-title");
+// ctitle3[0].style.color = "red";
+// ctitle3[1].style.color = "green";
+// ctitle3[2].style.color = "blue";
+// console.log(ctitle3);
+
+// document.querySelector(".this").style.color = "cyan";
+// document.querySelector(".this").style.background = "red";
+
+// console.log(document.querySelector(".card").getElementsByTagName("a"));
+// document.querySelector(".card").getElementsByTagName("a")[0].style.color = "pink";
+// document.querySelector(".card").getElementsByTagName("a")[0].style.background = "black";
+
+// console.log(document.getElementsByName("src"));
+
+console.log(document.getElementsByName("search"));
