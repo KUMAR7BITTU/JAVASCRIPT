d48eb30cf3d7ad61fa211a44afd2c87096af605b
KUMAR7BITTU <bittukumar30dec2002@gmail.com>
2025-11-18 10:54:53 +0530

    Table Based Navigation

---
+++ tut_35/script.js
@@ -0,0 +1,59 @@
+// let t = document.body.firstElementChild.firstElementChild;
+// console.log(t);
+// console.log(t.rows);
+// console.log(t.caption);
+// console.log(t.tHead);
+// console.log(t.tHead.firstElementChild);
+// console.log(t.tFoot);
+// console.log(t.tBodies);
+// console.log(t.tHead.rows);
+// console.log(t.tBodies[0].rows);
+
+// let arr = Array.from(t.tBodies);
+// console.log(arr);
+
+
+/*
+console.log(t.rows[0]);
+console.log(t.rows[0].cells) // cell -> collection of td and th .
+console.log(t.rows[0].rowIndex); // rowIndex -> Row number starting from 0 .
+console.log(t.rows[0].sectionRowIndex) // sectionRowIndex -> Index of tr inside enclosing element .
+console.log(t.rows[0].cells[0].cellIndex); // cellIndex -> no. of cell inside enclosing <tr> .
+console.log(t.rows[0].cells[1].cellIndex);
+console.log(t.rows[0].cells[2].cellIndex);
+console.log(t.rows[0].cells[3].cellIndex); 
+*/
+
+
+
+// console.log(t.rows[1]);
+// console.log(t.rows[1].cells);
+// console.log(t.rows[1].rowIndex);
+// console.log(t.rows[1].sectionRowIndex);
+// console.log(t.rows[1].cells[0]);
+// console.log(t.rows[1].cells[0].cellIndex); 
+// console.log(t.rows[1].cells[1].cellIndex);
+// console.log(t.rows[1].cells[2].cellIndex);
+// console.log(t.rows[1].cells[3].cellIndex);
+
+
+/* 
+console.log(t.rows[2]);
+console.log(t.rows[2].cells);
+console.log(t.rows[2].rowIndex);
+console.log(t.rows[2].sectionRowIndex) 
+*/
+
+/* 
+console.log(t.rows[3]);
+console.log(t.rows[3].cells);
+console.log(t.rows[3].rowIndex);
+console.log(t.rows[3].sectionRowIndex) 
+*/
+
+// print typeof document and typeof window in the console & see what it prints  ?
+
+console.log(typeof document);
+console.log(document);
+console.log(typeof window);
+console.log(window);
